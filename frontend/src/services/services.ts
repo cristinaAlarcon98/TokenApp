d691849829d1b04cf2908d50357a109c9c4ef0ae
Cristina Alarcon <cristina.jimenez@kreatize.com>
2023-08-23 20:54:22 +0200

    Fixed services

--- frontend/src/services/services.ts
+++ frontend/src/services/services.ts
@@ -2,11 +2,11 @@ import Axios from "axios";
 
 
 export const validateToken =  async (tokenValue: String)=> {
-    const response = await Axios.get('http://localhost:4000/api/validate?validateInputValue=${tokenValue}');
+    const response = await Axios.get(`http://localhost:4000/api/validate?validateInputValue=${tokenValue}`);
     return response.data
 }
 
 export const generateToken =  async (availableDigits: String)=> {
-    const response = await Axios.get('http://localhost:3000/api/generate?generateInputValue=${availableDigits}');
+    const response = await Axios.get(`http://localhost:3000/api/generate?generateInputValue=${availableDigits}`);
     return response.data
 }
