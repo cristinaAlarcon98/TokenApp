5f3b798501725ef30a11cd3250715e9ef734ff7b
Cristina Alarcon <cristina.jimenez@kreatize.com>
2023-08-23 20:39:30 +0200

    Fixed ports

--- frontend/src/services/services.ts
+++ frontend/src/services/services.ts
@@ -1,11 +1,12 @@
 import Axios from "axios";
 
+
 export const validateToken =  async (tokenValue: String)=> {
-    const response = await Axios.get(`${process.env.REACT_APP_VALIDATOR_URL}/api/validate?validateInputValue=${tokenValue}`);
+    const response = await Axios.get('http://localhost:4000/api/validate?validateInputValue=${tokenValue}');
     return response.data
 }
 
 export const generateToken =  async (availableDigits: String)=> {
-    const response = await Axios.get(`${process.env.REACT_APP_GENERATOR_URL}/api/generate?generateInputValue=${availableDigits}`);
+    const response = await Axios.get('http://localhost:3000/api/generate?generateInputValue=${availableDigits}');
     return response.data
 }
